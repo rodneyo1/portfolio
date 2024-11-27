2741b6c4aa50da64f19803e5c5deeb369507128e
rod31 <rodneyotieno27@gmail.com>
2024-11-28 00:47:28 +0300

    minor style changes

--- js/script.js
+++ js/script.js
@@ -25,32 +25,26 @@
         typeNextChar();
       }
       
-      // Usage example:
       const element = document.querySelector('.section-heading');
       if (element) {
         typeText(element, element.innerText, 100);
       }
 
 
-   
-    //   function addSlideTransition() {
-    //     const links = document.querySelectorAll("a");
-    
-    //     links.forEach(link => {
-    //         link.addEventListener("click", function(event) {
-    //             if (link.hostname === window.location.hostname && link.pathname !== window.location.pathname) {
-    //                 event.preventDefault();
-                    
-    //                 // Choose left or right based on preference
-    //                 document.body.classList.add("slide-out-left");
-    
-    //                 setTimeout(() => {
-    //                     window.location = link.href;
-    //                 }, 500);
-    //             }
-    //         });
-    //     });
-    // }
-    
-    // addSlideTransition();
-    
+      //save scroll position
+      document.addEventListener("DOMContentLoaded", () => {
+        // Generate a unique key for the current page
+        const pageKey = `scrollPosition_${window.location.pathname}`;
+      
+        // Save scroll position before navigating away
+        window.addEventListener("beforeunload", () => {
+          localStorage.setItem(pageKey, window.scrollY);
+        });
+      
+        // Restore scroll position when the page is loaded
+        const savedScrollPosition = localStorage.getItem(pageKey);
+        if (savedScrollPosition !== null) {
+          window.scrollTo(0, parseInt(savedScrollPosition, 10));
+        }
+      });
+      
